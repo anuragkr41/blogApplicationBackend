806b518643737bc9d8ce6139efcdf54f0cac1484
Anurag Kumar <anuragkr41@gmail.com>
2023-05-25 00:24:25 +0530

    Created Forms

--- BlogApiFrontEnd/blog-app/src/components/Pages/Login.js
+++ BlogApiFrontEnd/blog-app/src/components/Pages/Login.js
@@ -1,10 +1,54 @@
+import {
+	Button,
+	Card,
+	CardBody,
+	CardHeader,
+	Col,
+	Container,
+	Form,
+	FormGroup,
+	Input,
+	Label,
+	Row,
+} from "reactstrap";
 import Base from "../Base";
 
 const Login = () => {
 	return (
 		<Base>
-			<h1>This is my login component</h1>
-			<p> Welcome to login</p>
+			<Container>
+				<Row className="mt-4">
+					<Col
+						sm={{
+							size: 6,
+							offset: 3,
+						}}
+					>
+						<Card>
+							<CardHeader>
+								<h3>Login Here!!</h3>
+							</CardHeader>
+							<CardBody>
+								<Form>
+									<FormGroup>
+										<Label for="Email">Enter Email</Label>
+										<Input type="text" id="email " />
+										<Label for="password">Enter Password</Label>
+										<Input type="password" id="password" />
+									</FormGroup>
+
+									<Container className="text-center ">
+										<Button color="dark">Login</Button>
+										<Button className="ms-2" color="secondary">
+											Reset
+										</Button>
+									</Container>
+								</Form>
+							</CardBody>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
 		</Base>
 	);
 };
--- BlogApiFrontEnd/blog-app/src/components/Pages/SignUp.js
+++ BlogApiFrontEnd/blog-app/src/components/Pages/SignUp.js
@@ -1,10 +1,79 @@
+import {
+	Button,
+	Card,
+	CardBody,
+	CardHeader,
+	Col,
+	Container,
+	Form,
+	FormGroup,
+	Input,
+	Label,
+	Row,
+} from "reactstrap";
 import Base from "../Base";
 
 const SignUp = () => {
 	return (
 		<Base>
-			<h1>This is my SignUp page</h1>
-			<p> Welcome to Signup page</p>
+			<Container>
+				<Row className="mt-4">
+					<Col sm={{ size: 6, offset: 3 }}>
+						<Card outline="dark" color="primary">
+							<CardHeader>
+								<h3>Fill Information to register!!</h3>
+							</CardHeader>
+							<CardBody>
+								<Form>
+									<FormGroup>
+										<Label for="name">Enter Name</Label>
+										<Input
+											type="text"
+											placeholder="Enter your name"
+											id="name"
+										/>
+									</FormGroup>
+
+									<FormGroup>
+										<Label for="email">Enter Email</Label>
+										<Input
+											type="email"
+											placeholder="Enter your email"
+											id="email"
+										></Input>
+									</FormGroup>
+
+									<FormGroup>
+										<Label for="password">Enter Password</Label>
+										<Input
+											type="password"
+											placeholder="Enter your password"
+											id="password"
+										></Input>
+									</FormGroup>
+
+									<FormGroup>
+										<Label for="about">About yourself</Label>
+										<Input
+											type="textarea"
+											placeholder="Tell us about yourself"
+											id="about"
+											style={{ height: "250px" }}
+										></Input>
+									</FormGroup>
+
+									<Container className="text-center">
+										<Button color="dark">Register</Button>
+										<Button color="secondary" className="ms-2">
+											Reset
+										</Button>
+									</Container>
+								</Form>
+							</CardBody>
+						</Card>
+					</Col>
+				</Row>
+			</Container>
 		</Base>
 	);
 };
